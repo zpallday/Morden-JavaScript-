db8c5d4e5f7c27fffc6bef6a8958972b3059f042
Zach <zpman95@hotmail.com>
2020-07-30 12:05:37 -0500

    variables

---
+++ JS Values/Variables.js
@@ -0,0 +1,45 @@
+// let age = 43 
+// console.lot(age)
+
+// let numOfHens = 6;
+
+// let avgRating = 9.7;
+
+// avgRating - numOfHens
+
+// var firstName = 'John';
+// var age = 28;
+// // Type coercion
+// console.log(firstName + ' ' + age);
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+// // Variable mutation
+// age = 'twenty eight';
+// job = 'driver';
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+// var lastName = prompt('What is his last Name?');
+// console.log(firstName + ' ' + lastName);
+// */
+
+
+// const hens = 4 
+// hens = 20
+
+// const year = 1973 
+// year  = 2010
+
+// var x = 5
+// var z = 6
+// var y = x + z 
+
+// let x = 3
+
+// if (x === 3) {
+//     let x = 6;
+
+//     console.log(x)
+// }
+
+// console.log(x)
